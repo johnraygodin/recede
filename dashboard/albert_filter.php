850e3786ee363697de32d2b39a8cf7c7274ddb69
johnraygodin <89831641+johnraygodin@users.noreply.github.com>
2023-06-08 16:03:40 +0800

    Create albert_filter.php

---
+++ dashboard/albert_filter.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="search.css" media="screen">
+	<title>Search</title>
+</head>
+<body>
+	<div class="search">
+		<form method="GET">
+			<input type="text" <?php if(isset($_GET['search'])){echo $_GET['search']; } ?> placeholder="YYYY-MM-DD" name="search">
+			<button class="btnsearch" type="submit">Search</button> 
+		</form>
+	<div class="sfunction">
+		<table class="table" cellspacing="3" cellpadding="3">
+			 <thead>
+             <tr>
+                     <th>ID</th>
+                     <th>VALUE</th>
+                     <th>CREATED</th>
+             </tr>
+             </thead> 
+                    <tbody>
+                                <?php 
+                                    $con = mysqli_connect("localhost","root","secret","esp32");
+
+                                    if(isset($_GET['search']))
+                                    {
+                                        $filtervalues = $_GET['search'];
+                                        $query = "SELECT * FROM albert_close_contact WHERE CONCAT(id,value,created) LIKE '%$filtervalues%'  ";
+                                        $query_run = mysqli_query($con, $query);
+
+                                        if(mysqli_num_rows($query_run) > 0)
+                                        {
+                                            foreach($query_run as $items)
+                                            {
+                                                ?>
+                                                <tr>
+                                                    <td><?= $items['ID']; ?></td>
+                                                    <td><?= $items['value']; ?></td>
+                                                    <td><?= $items['Created']; ?></td>
+                                                </tr>
+                                                <?php
+                                            }
+                                        }
+                                        else
+                                        {
+                                            ?>
+                                                <tr>
+                                                    <td colspan="4">No Record Found</td>
+                                                </tr>
+                                            <?php
+                                        }
+                                    }
+                                ?>
+                            </tbody>
+		</table>
+	</div>
+</body>
+</html>

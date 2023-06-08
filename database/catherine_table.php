a1a1b4c4d2c3baf8d5b7f91b9e79782cefd07177
johnraygodin <89831641+johnraygodin@users.noreply.github.com>
2023-06-08 16:20:28 +0800

    Create catherine_table.php

---
+++ database/catherine_table.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "secret";
+$dbname = "esp32";
+
+$value = "";
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+	$value = $_POST["value"];
+
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	
+	if($conn->connect_error){
+	die("Connection failed: " . $conn->connect_error);
+	}
+	$sql = "INSERT INTO catherine_close_contact (value) VALUES ('" . $value ."')";
+
+	if ($conn->query($sql) == TRUE){
+		echo"New record created successfully";
+	}else{
+		echo"Error: " . $sql . "<br>" . $conn->error;
+	}
+	
+	$conn->close();		
+}else{
+echo"No data posted with HTTP POST.";
+}
+
+?>
